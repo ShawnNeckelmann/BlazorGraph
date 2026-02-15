cccfeb465d760760f91a134bf9b2746986e5092d
Shawn Neckelmann <shawn@statrecovery.com>
2026-02-14 23:15:33 -0600

    Providing the user recommendation service.  Provide a username & get a list of vertex "bought".

---
+++ src/BlazorGraph.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Process.Traversal;
+using static Gremlin.Net.Process.Traversal.AnonymousTraversalSource;
+
+namespace BlazorGraph.Persistence.Repositories;
+
+public class UserRepository(GremlinClient client)
+{
+    private readonly GraphTraversalSource _g = Traversal().With(new DriverRemoteConnection(client));
+
+    public async Task<IList<string?>> GetRecommendationsAsync(string userName)
+    {
+        return await _g.V().Has("person", "name", userName)
+            .As("self") // Label the starting user
+            .Out("purchased").Aggregate("bought") // Collect what they already own
+            .In("purchased") // Find peers who bought the same things
+            .Where(P.Not(P.Eq("self"))) // Don't look at Lilou herself as a peer
+            .Out("purchased") // Find what those peers bought
+            .Where(P.Without("bought")) // Filter out what Lilou already owns
+            .Dedup() // Remove duplicates
+            .Values<string>("name") // Get the product names
+            .Promise(t => t.ToList());
+    }
+}
