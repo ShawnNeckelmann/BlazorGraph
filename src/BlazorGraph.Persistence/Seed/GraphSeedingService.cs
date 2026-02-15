d4fcf55f55a00132a644764dd80bda80a9e83a84
Shawn Neckelmann <shawn@statrecovery.com>
2026-02-14 23:05:54 -0600

    Using chaining logic instead of going to the database 3 times.

--- src/BlazorGraph.Persistence/Seed/GraphSeedingService.cs
+++ src/BlazorGraph.Persistence/Seed/GraphSeedingService.cs
@@ -22,9 +22,11 @@ public class GraphSeedingService(GremlinClient client)
         var p2 = await _g.AddV("product").Property("name", "Sticker Set").Promise(t => t.Next());
 
         // 3. Add Edges
-        await _g.V(lp).AddE("purchased").To(p1).Promise(t => t.Iterate());
-        await _g.V(bp).AddE("purchased").To(p1).Promise(t => t.Iterate());
-        await _g.V(bp).AddE("purchased").To(p2).Promise(t => t.Iterate());
+        await _g
+            .V(lp).AddE("purchased").To(p1)
+            .V(bp).AddE("purchased").To(p1)
+            .V(bp).AddE("purchased").To(p2)
+            .Promise(t => t.Iterate());
 
         // 4. Verify by counting vertices
         var count = await _g.V().HasLabel("person").Count().Promise(t => t.Next());
